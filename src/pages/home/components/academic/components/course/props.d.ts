ff888e34ce61b6b205074ffd87a12f79b6a10dcd
Leandro Farias <leandro.farias01@outlook.com>
2023-03-23 15:51:57 -0300

    refactor: folder rename

--- src/pages/home/components/academic/components/course/props.d.ts
+++
@@ -1,6 +0,0 @@
-import { ICourse } from 'src/core/models/ICourse';
-
-export interface ICourseProps {
-  card: ICourse;
-  index: number;
-}
--- src/pages/home/components/academic/components/extracurricular/props.d.ts
+++
@@ -1,6 +0,0 @@
-import { IExtracurricular } from 'src/core/models';
-
-export interface ICardProps {
-  card: IExtracurricular;
-  index: number;
-}
